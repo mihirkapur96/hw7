bd6af34aefda1b7664a49d690d1108c523f2a085
Mihir Kapur <mihirkapur96@gmail.com>
2021-03-23 12:55:38 -0700

    First Release

---
+++ JS/ex2.js
@@ -0,0 +1,16 @@
+const countryList = [
+    "Afghanistan" ,
+    "Albania" ,
+    "Algeria" ,
+    "Andorra" ,
+    "Angola" ,
+    "Anguilla" ,
+    "Antarctica" ,
+    "Antigua-and-Barbuda" ,
+    "Argentina" ,
+    "Armenia" ,
+    "Aruba" ,
+    "Australia",
+    "Autria" ,
+    "Azerbaïjan" ,
+];
